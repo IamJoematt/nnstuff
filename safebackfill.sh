44a0b07badbe40a6e57307f282bd28341a6aa82a
Thracky <thracky@gmail.com>
2013-01-08 16:43:49 -0500

    Added update_binaries to maintain new releases

--- safebackfill.sh
+++ safebackfill.sh
@@ -15,7 +15,6 @@ export PHP_PATH="/usr/bin/php"
 #Path to your newznab installation
 export NEWZNAB_DIR="/var/www/newznab"
 
-
 #Number of days to backfill.
 BACKFILL_DAYS="200"
 
@@ -25,6 +24,9 @@ MYSQL_USER="root"
 MYSQL_PASS=""
 MYSQL_HOST="localhost"
 
+#Enter number of times to allow update_releases to loop while processing backfill before doing an update_binaries.
+BINARIES_KEEPUP="5"
+
 #Begin script
 while :
 
@@ -32,22 +34,39 @@ do
 
 cd ${NEWZNAB_DIR}/misc/update_scripts
 
+#Get our groups up to date before starting a backfill day, threaded or not.
+#${PHP_PATH} ${NEWZNAB_DIR}/misc/update_scripts/update_binaries.php
+${PHP_PATH} ${NEWZNAB_DIR}/misc/update_scripts/update_binaries_threaded.php
+${PHP_PATH} ${NEWZNAB_DIR}/misc/update_scripts/update_releases.php
+
 #Run backfill, either regular or threaded, take your pick.
 #${PHP_PATH} ${NEWZNAB_DIR}/misc/update_scripts/backfill.php
 ${PHP_PATH} ${NEWZNAB_DIR}/misc/update_scripts/backfill_threaded.php
-
-
 
 #Run update_releases once so we know how many releases we are dealing with.
 ${PHP_PATH} ${NEWZNAB_DIR}/misc/update_scripts/update_releases.php
 
 #Check the number of releases waiting for postproc
 RELEASE_BACKLOG=`mysql -u $MYSQL_USER -h $MYSQL_HOST -p$MYSQL_PASS $MYSQL_DBNAME -s -N -e "select COUNT(*) from releases r left join category c on c.ID = r.categoryID where (r.passwordstatus between -6 and -1) or (r.haspreview = -1 and c.disablepreview = 0)"`
+LOOPCOUNTER = 0
 
 while [ $RELEASE_BACKLOG -gt 10 ]; do 
 ${PHP_PATH} ${NEWZNAB_DIR}/misc/update_scripts/update_releases.php
 
 RELEASE_BACKLOG=`mysql -u $MYSQL_USER -h $MYSQL_HOST -p$MYSQL_PASS $MYSQL_DBNAME -s -N -e "select COUNT(*) from releases r left join category c on c.ID = r.categoryID where (r.passwordstatus between -6 and -1) or (r.haspreview = -1 and c.disablepreview = 0)"`
+LOOPCOUNTER = $LOOPCOUNTER + 1
+if [ "$LOOPCOUNTER" -gt "$BINARIES_KEEPUP" ]
+then
+
+#Update binaries and process releases every time update_releases loops the number of times defined in BINARIES_KEEPUP
+#Again, choose threaded or not.
+
+#${PHP_PATH} ${NEWZNAB_DIR}/misc/update_scripts/update_binaries.php
+${PHP_PATH} ${NEWZNAB_DIR}/misc/update_scripts/update_binaries_threaded.php
+${PHP_PATH} ${NEWZNAB_DIR}/misc/update_scripts/update_releases.php
+
+fi
+
 done
 
 echo "Finished backfilling another day, running optimizations and cleanup."
